8586c30a14f1f3ea8e2ab702020843200ec757b6
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-10-24 22:28:17 +0100

    Initial commit

---
+++ Models/WalletAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZedCrestBankApi.Models
+{
+    [Table("Wallets")]
+    public class WalletAccount
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string WalletName { get; set; }
+        public decimal CurrentWalletBalance { get; set; }
+        public WalletType WalletType { get; set; }
+        public string AccountNumberGenerator { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PinSalt { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+
+        Random rand = new Random();
+
+        public WalletAccount() 
+        { 
+            AccountNumberGenerator = Convert.ToString((long)rand.NextDouble() * 9_000_000_000L + 1_000_000_000L);
+            WalletName = $"{FirstName} {LastName}";
+        }
+
+
+    }
+
+    
+
+    public enum WalletType
+    {
+        USD,
+        NGN
+    }
+}
---
+++ ZDbContext/ZedCrestBankingApiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZedCrestBankApi.Models;
+
+namespace ZedCrestBankApi.ZDbContext
+{
+    public class ZedCrestBankingApiContext : DbContext
+    {
+        public ZedCrestBankingApiContext(DbContextOptions<ZedCrestBankingApiContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<WalletAccount> WalletAccounts { get; set; }
+        public DbSet<Transcation> Transcations { get; set; }
+    }
+}
