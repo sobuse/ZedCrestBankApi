087416fe93edc4c74b9a875e81c1827bd3864064
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-10-31 07:07:38 +0100

    Added interface

---
+++ Services/Interface/ITransactionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZedCrestBankApi.Models;
+
+namespace ZedCrestBankApi.Services.Interface
+{
+    public interface ITransactionService
+    {
+        Response CreateNewTranscation(Transaction transcation);
+        Response FindTransactionByDate(DateTime date);
+        Response MakeDeposit(string WalletAccountNumber, decimal Amount, string TransactionPin);
+        Response MAkeWithdrawal(string WalletAccountNumber, decimal Amount, string TransactionPin);
+        Response MakeFundTransfer(string FromWalletAccount, string ToWalletAccount, decimal Amount, string TransactionPin);
+
+    }
+}
---
+++ Services/Interface/IWalletService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZedCrestBankApi.Models;
+
+namespace ZedCrestBankApi.Services
+{
+    public interface IWalletService
+    {
+        WalletAccount Authenticate(string WalletAccountNumber, string Pin);
+        IEnumerable<WalletAccount> GetAllAccounts();
+        WalletAccount Create(WalletAccount wallet, string Pin, string ComfirmPin);
+        void Update(WalletAccount wallet, string Pin = null);
+        void Delete( int Id);
+        WalletAccount GetById(int Id);
+        WalletAccount GetByWalletAccountNumber(string WalletAccountNumber);
+
+    }
+}
