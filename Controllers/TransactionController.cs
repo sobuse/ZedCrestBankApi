f5236397f8bcdba49d712c2b2678c77e484bef24
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-10-31 16:05:50 +0100

    created Transaction controller and service class

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZedCrestBankApi.Services;
+
+namespace ZedCrestBankApi.Controllers
+{
+    public class TransactionController : ControllerBase
+    {
+        private IWalletService _walletService;
+        IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public TransactionController(IWalletService walletService, IMapper mapper, IMediator mediator)
+        {
+            _walletService = walletService;
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("createNewTrasaction")]
+        public IActionResult CreateNewTransaction([FromBody] TransactionRequestDto  transactionRequestDto)
+        {
+            return Ok();
+        }
+    }
+}
--- Services/Implementation/TransactionService.cs
+++ Services/Implementation/TransactionService.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ZedCrestBankApi.Models;
+using ZedCrestBankApi.Services.Interface;
+using ZedCrestBankApi.Utilities;
+using ZedCrestBankApi.ZDbContext;
 
 namespace ZedCrestBankApi.Services.Implementation
 {
-    public class TransactionService
+    public class TransactionService : ITransactionService
     {
+        private ZedCrestBankingApiContext _dbContext;
+        ILogger<TransactionService> _logger;
+        private AppSettings  _appSetting;
+
+        public TransactionService(ZedCrestBankingApiContext dbContext) => _dbContext = dbContext;
+
+
+        public Response CreateNewTranscation(Transaction transcation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response FindTransactionByDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response MakeDeposit(string WalletAccountNumber, decimal Amount, string TransactionPin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response MakeFundTransfer(string FromWalletAccount, string ToWalletAccount, decimal Amount, string TransactionPin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response MAkeWithdrawal(string WalletAccountNumber, decimal Amount, string TransactionPin)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
